6177b591404f36eb1ccdd49bf462bf37c1bb3348
Samuel Alonso <Samuelalonso939@gmail.com>
2024-01-01 16:45:53 -0400

    fact transformation
    feliz ano

--- Datawarehouse/database.sql
+++ Datawarehouse/database.sql
@@ -5,13 +5,6 @@ CREATE TABLE IF NOT EXISTS dim_tiempo(
     dia int not null
 );
 
-CREATE TABLE IF NOT EXISTS dim_empleado(
-    cs_empleado serial primary key not null,
-    id_empleado text not null,
-    nombre varchar(150) not null,
-    cargo varchar(20) not null
-);
-
 CREATE TABLE IF NOT EXISTS dim_cliente(
     cs_cliente serial primary key not null,
     id_cliente text not null,
@@ -39,12 +32,11 @@ CREATE TABLE IF NOT EXISTS dim_factura(
 
 CREATE TABLE IF NOT EXISTS ventas_diarias(
     cs_tiempo int not null references dim_tiempo (cs_tiempo),
-    cs_empleado int not null references dim_empleado (cs_empleado),
     cs_cliente int not null references dim_cliente (cs_cliente),
     cs_sucursal int not null references dim_sucursal (cs_sucursal),
     cs_producto int not null references dim_producto (cs_producto),
     cs_factura int not null REFERENCES dim_factura(cs_factura),
     ventas_totales decimal (15,2) not null,
     cantidad_vendida int not null,
-    constraint pk_ventas primary key (cs_factura,cs_sucursal,cs_empleado,cs_tiempo,cs_cliente,cs_producto) 
+    constraint pk_ventas primary key (cs_factura,cs_sucursal,cs_tiempo,cs_cliente,cs_producto) 
 );
